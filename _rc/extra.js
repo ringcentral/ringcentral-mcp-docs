3909efecd43a831eaff2ed8e2875cecc86ae9e1f
DaKingKong <da.kong@ringcentral.com>
2026-05-12 13:18:47 +0800

    Deployed a1c82e1 with MkDocs version: 1.6.1

--- _rc/extra.js
+++ _rc/extra.js
@@ -1,5 +1,22 @@
 console.log('extra.js loaded');
-if (window.location.origin === 'https://ringcentral.github.io') {
-  const newUri = window.location.href.replace('ringcentral.github.io/rc-unified-crm-extension', 'appconnect.labs.ringcentral.com');
-  window.location.href = newUri;
-}
+(function () {
+  const legacyHost = 'ringcentral.github.io';
+  const legacyPath = '/rc-unified-crm-extension';
+  const canonicalHost = 'appconnect.labs.ringcentral.com';
+
+  if (window.location.hostname !== legacyHost) {
+    return;
+  }
+
+  if (!window.location.pathname.startsWith(legacyPath)) {
+    return;
+  }
+
+  const nextUrl = new URL(window.location.href);
+  nextUrl.hostname = canonicalHost;
+  nextUrl.pathname = nextUrl.pathname.slice(legacyPath.length) || '/';
+
+  if (nextUrl.href !== window.location.href) {
+    window.location.replace(nextUrl.href);
+  }
+})();
